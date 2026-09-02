9a46f628ec3311b6c0ea09c3d01bc733d6b5e6da
labsfiap <labsfiap@PA709MICRO24>
2026-09-02 11:27:13 -0300

    Add project files.

---
+++ CPCauaRochaClasses/Jogo.cs
@@ -0,0 +1,40 @@
+﻿// Cauã Rocha do Amaral
+// RM567593
+// 2tdspa
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPCauaRochaClasses
+{
+    public class Jogo
+    {
+        String nome;
+        int dataDeLancamento;
+        String desenvolvedora;
+        int classificacaoIndicativa;
+        String genero;
+        double preco;
+
+        public Jogo(String nome, int dataDeLancamento, String desenvolvedora, int classificacaoIndicativa, String genero, double preco){
+
+            this.nome= nome;
+            this.dataDeLancamento= dataDeLancamento;
+            this.desenvolvedora= desenvolvedora;
+            this.classificacaoIndicativa= classificacaoIndicativa;
+            this.genero= genero;    
+            this.preco= preco;
+
+        }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Data de Lançamento: {dataDeLancamento}");
+            Console.WriteLine($"Desenvolvedora: {desenvolvedora}");
+            Console.WriteLine($"Classificação Indicativa: {classificacaoIndicativa}");
+            Console.WriteLine($"Genero: {genero}");
+            Console.WriteLine($"Preço: {preco}");
+
+        }
+    }
+}
---
+++ CPCauaRochaClasses/JogoTerror.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPCauaRochaClasses
+{
+    public class JogoTerror : Jogo{
+
+        String tipoDeTerror;
+        int nivelDeMedo;
+        bool possuiJumpscare;
+
+    }
+  
+}
